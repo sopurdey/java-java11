25f8ba8b4189458feff16288272623c91e8bb548
sopurdey <a_purdey@hotmail.com>
2021-11-25 16:34:59 +0100

    TP Date/Time fin ex07[C

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,8 +3,10 @@ package java8.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.ZoneId;
 import java.util.Date;
 
 import org.junit.Test;
@@ -17,17 +19,17 @@ public class DateAndTime_07_Test {
 	@Test
 	public void test_date_to_localdate() throws Exception {
 
-		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		// Créer une date Java 1 (12/02/2017)
+		Date date = new Date(117, 1, 12);
 
-		// TODO transformer la date en Instant
+		// Transformer la date en Instant
+		Instant instant = date.toInstant();
+		
 
-		// TODO transformer la date en LocalDate
+		// Transformer la date en LocalDate
+		LocalDate result = LocalDate.ofInstant(instant, ZoneId.systemDefault());
 
-		LocalDate result = null;
-
-		// TODO valoriser les différentes variables afin de rendre le test passant
-
+		// Valoriser les différentes variables afin de rendre le test passant
 		assertThat(result.getYear(), is(2017));
 		assertThat(result.getMonth(), is(Month.FEBRUARY));
 		assertThat(result.getDayOfMonth(), is(12));

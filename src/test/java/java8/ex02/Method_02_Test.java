2d31243ca1f9add25bffdc666c28bfdec360339b
sopurdey <a_purdey@hotmail.com>
2021-11-26 10:08:29 +0100

    TP default methods

--- src/test/java/java8/ex02/Method_02_Test.java
+++ src/test/java/java8/ex02/Method_02_Test.java
@@ -12,42 +12,53 @@ import java8.data.Person;
  */
 public class Method_02_Test {
 
-    // tag::IDao[]
-    interface IDao {
-        List<Person> findAll();
+	// tag::IDao[]
+	interface IDao {
+		List<Person> findAll();
 
-        // TODO créer une méthode String format()
-        // TODO la méthode retourne une chaîne de la forme [<nb_personnes> persons]
-        // TODO exemple de résultat : "[14 persons]", "[30 persons]"
-    }
-    // end::IDao[]
+		// Créer une méthode String format()
+		// La méthode retourne une chaîne de la forme [<nb_personnes> persons]
+		// exemple de résultat : "[14 persons]", "[30 persons]"
+		public default String format() {
+			List<Person> people = findAll();
+			int nb_personnes = 0;
+			for (int i = 0; i < people.size(); i++) {
+				nb_personnes += 1;
+			}
+			return "[" + nb_personnes + " persons]";
+		}
+	}
+	// end::IDao[]
 
-    // tag::DaoA[]
-    class DaoA implements IDao {
+	// tag::DaoA[]
+	class DaoA implements IDao {
 
-        List<Person> people = Data.buildPersonList(20);
+		List<Person> people = Data.buildPersonList(20);
 
-        @Override
-        public List<Person> findAll() {
-            return people;
-        }
+		@Override
+		public List<Person> findAll() {
+			return people;
+		}
 
-        // TODO redéfinir la méthode String format()
-        // TODO la méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
-        // TODO exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
-        // TODO l'implémentation réutilise la méthode format() de l'interface
+		// Redéfinir la méthode String format()
+		// La méthode retourne une chaîne de la forme DaoA[<nb_personnes> persons]
+		// exemple de résultat : "DaoA[14 persons]", "DaoA[30 persons]"
+		// L'implémentation réutilise la méthode format() de l'interface
+		@Override
+		public String format() {
+			return "DaoA" + IDao.super.format();
+		}
+	}
+	// end::DaoA[]
 
-    }
-    // end::DaoA[]
+	@Test
+	public void test_daoA_format() throws Exception {
 
-    @Test
-    public void test_daoA_format() throws Exception {
+		DaoA daoA = new DaoA();
 
-        DaoA daoA = new DaoA();
+		// Invoquer la méthode format() pour que le test soit passant
+		String result = daoA.format();
 
-        // TODO invoquer la méthode format() pour que le test soit passant
-        String result = null;
-
-        assert "DaoA[20 persons]".equals(result);
-    }
+		assert "DaoA[20 persons]".equals(result);
+	}
 }
